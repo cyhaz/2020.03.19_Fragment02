6957ac055799e6af72f5d8d94a339b20cc4f6836
cyhaz <cyhaz@naver.com>
2020-03-19 14:39:09 +0900

    방 목록 프래그먼트 생성

---
+++ app/src/main/java/kr/co/youhyun/a20200319_fragment02/fragments/RoomListFragment.java
@@ -0,0 +1,33 @@
+package kr.co.youhyun.a20200319_fragment02.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import kr.co.youhyun.a20200319_fragment02.R;
+import kr.co.youhyun.a20200319_fragment02.databinding.FragmentRoomListBinding;
+
+public class RoomListFragment extends Fragment {
+
+    FragmentRoomListBinding binding = null;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_room_list, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//
+    }
+}
